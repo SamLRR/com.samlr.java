2a0d5b392daf3c40cc215215ded76b1b6d2b4a66
user <samatov_lr@mail.ru>
2018-03-01 09:37:14 +0300

    01032018 refactor after Igor correct

--- src/khasang/level1/battleship/Ship.java
+++ src/khasang/level1/battleship/Ship.java
@@ -1,33 +1,35 @@
 package khasang.level1.battleship;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Ship {
-    private ArrayList<Point> cells = new ArrayList<Point>();
+    private List<Point> cells = new ArrayList<Point>();
     /**
      * Длина корабля
      */
-    private int lenghtOfShip;
+    private int lengthOfShip;
     /**
      * Начальные координаты корабля
      */
-    Point point;
+    private Point point;
 
-    public ArrayList<Point> getCells() {
-        return cells;
+    public List<Point> getCells() {
+        return Collections.unmodifiableList(cells);
     }
 
-    public int getLenghtOfShip() {
-        return lenghtOfShip;
+    public int getLengthOfShip() {
+        return lengthOfShip;
     }
 
     public Ship(int lenghtOfShip, Point point, int direction) {
-        this.lenghtOfShip = lenghtOfShip;
-        this.point = point;
-        for (int i = 0; i < lenghtOfShip; i++)
-            cells.add(
-                    new Point(point.getX() + i * ((direction == 1) ? 0 : 1),
-                            point.getY() + i * ((direction == 1) ? 1 : 0)));
+        this.lengthOfShip = lenghtOfShip;
+        for (int i = 0; i < lenghtOfShip; i++) {
+            int x = point.getX() + i * ((direction == 1) ? 0 : 1);
+            int y = point.getY() + i * ((direction == 1) ? 1 : 0);
+            cells.add(new Point(x, y));
+        }
     }
 
     boolean isOutOfField(int bottom, int top) {
@@ -48,10 +50,11 @@ public class Ship {
     boolean isOverlayOrTouchCell(Point ctrlCell) {
         for (Point cell : cells)
             for (int dx = -1; dx < 2; dx++)
-                for (int dy = -1; dy < 2; dy++)
+                for (int dy = -1; dy < 2; dy++) {
                     if (ctrlCell.getX() == cell.getX() + dx &&
                             ctrlCell.getY() == cell.getY() + dy)
                         return true;
+                }
         return false;
     }
 }
